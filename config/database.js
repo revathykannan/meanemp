2f885255f717d2fc641c5642d601c35cfa0d8acd
revathykannan <33889714+revathykannan@users.noreply.github.com>
2017-12-05 12:56:50 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const crypto = require('crypto').randomBytes(256).toString('hex'); //provides cr
 
 //Export config object
 module.exports = {
-    uri= 'mongodb://localhost:27017/employees', //Database URI and database name
+   // uri= 'mongodb://localhost:27017/employees', //Database URI and database name
     uri= 'mongodb://<dbuser>:<dbpassword>@ds123896.mlab.com:23896/employees', //production
     secret: crypto, //Crypto-created secret
     db: 'employees' //Database name
